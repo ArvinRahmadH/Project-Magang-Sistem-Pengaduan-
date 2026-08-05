80866bb792fa3072d9296ff8a1ce23b116c2bdc7
ArvinRahmadH <arvinrahmadh@gmail.com>
2026-08-05 13:04:02 +0700

    Perbaikan logo favicon

--- resources/views/admin/notes/master.blade.php
+++ resources/views/admin/notes/master.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Admin Dashboard - Management Pengaduan</title>
-  <link rel="icon" type="image/png" href="{{ asset('images/Logo_Polres_Malang-favicon.png') }}">
+  <link rel="icon" type="image/png" href="{{ asset('images/Logo_Polres_Malang.png') }}">
   <link rel="shortcut icon" type="image/png" href="{{ asset('images/Logo_Polres_Malang.png') }}">
   
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
