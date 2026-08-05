1d444e940bb6c6e539ffdcccb686557332da82f9
ArvinRahmadH <arvinrahmadh@gmail.com>
2026-08-05 12:52:36 +0700

    Edit Master Blade

--- resources/views/admin/notes/master.blade.php
+++ resources/views/admin/notes/master.blade.php
@@ -5,9 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Admin Dashboard - Notes Management</title>
+  <title>Admin Dashboard - Management Pengaduan</title>
   <link rel="icon" type="image/png" href="{{ asset('images/Logo_Polres_Malang-favicon.png') }}">
-  <link rel="shortcut icon" type="image/png" href="{{ asset('images/logo_fix_malang.png') }}">
+  <link rel="shortcut icon" type="image/png" href="{{ asset('images/Logo_Polres_Malang.png') }}">
   
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
