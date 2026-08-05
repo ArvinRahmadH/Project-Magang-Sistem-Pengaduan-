0d6569217b11afa31def9ae0cb5d8b2170e24a82
ArvinRahmadH <arvinrahmadh@gmail.com>
2026-08-05 13:08:46 +0700

    Perbaikan logo favicon login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login - Fix Malang</title>
+    <title>Administrator Login</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/Logo_Polres_Malang.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
